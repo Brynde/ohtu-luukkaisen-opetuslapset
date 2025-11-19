61a4f1112d0c9237f3625f19d894892421383627
kasperikpnn <kasperikpnn@gmail.com>
2025-11-19 15:43:30 +0200

    implemented editing book info

--- src/repositories/book_repository.py
+++ src/repositories/book_repository.py
@@ -9,7 +9,29 @@ def get_books():
     books = result.fetchall()
     return [Book(book[0], book[1], book[2]) for book in books] 
 
-def create_book(content):
-    sql = text("INSERT INTO books (content) VALUES (:content)")
-    db.session.execute(sql, { "content": content })
+def get_info(book_id):
+    sql = text("SELECT author, title, year FROM books WHERE id = :id")
+    result = db.session.execute(sql, {"id": book_id})
+    book_info = result.fetchone()
+    if book_info is None:
+        return None
+    return book_info
+
+def create_book(author, title, year):
+    sql = text("INSERT INTO books (author, title, year) VALUES (:author, :title, :year)")
+    db.session.execute(sql, {"author": author, "title": title, "year": year})
     db.session.commit()
+
+def edit_book(book_id, content):
+    current_info = get_info(book_id)
+    i=0
+    new_info = False
+    for info in content:
+        if info != current_info[i]:
+            new_info = True
+            current_info[i] = info
+        i+=1
+    if new_info:
+        sql = text(f"UPDATE books SET author = :author, title = :title, year = :year WHERE id = :id")
+        db.session.execute(sql, {"author": current_info[0], "title": current_info[1], "year": current_info[2], "id": book_id})
+        db.session.commit()
